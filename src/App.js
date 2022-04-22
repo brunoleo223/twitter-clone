a06cd6c882d987832af98fafb88271a3c48dd6ee
brunoleo223 <brunoleo223@gmail.com>
2022-04-21 23:06:43 -0300

    test git

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+    <h1>Vamos lá Brunão, seu primeiro clone</h1>
+    
+    {/* Sidebar */}
+    <Sidebar />
+    
+    {/* Feed */}
+    
+    {/* Widgets */}
+    
+    </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './Sidebar.css';
+import SidebarOption from "./SidebarOption";
+
+import HomeIcon from '@mui/icons-material/Home';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+function Sidebar(){
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon />
+
+            {/* SidebarOption */}
+            <SidebarOption Icon={HomeIcon} text="Home" />
+            <SidebarOption Icon={SearchIcon} text="Explore" />
+            <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+            <SidebarOption Icon={MailOutlineOutlinedIcon} text="Messages" />
+            <SidebarOption Icon={BookmarkBorderOutlinedIcon} text="Bookmarks" />
+            <SidebarOption Icon={ListAltIcon} text="Lists" />
+            <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+            <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+            {/* Button -> Tweet */}
+        </div>
+    );
+}
+
+export default Sidebar;
